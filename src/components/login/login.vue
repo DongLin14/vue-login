da8d279626cf5339fe423a770fb72017a1ea9c72
Your Name <you@example.com>
2020-09-23 19:43:35 +0800

    第一次提交 9.23 19:43

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>login</div>
+  <div>
+
+    <el-form
+      label-position="left"
+      label-width="80px"
+      :model="formdata">
+
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button type="primary" round>登录</el-button>
+    </el-form>
+
+  </div>
 </template>
 
 <script>
-  export default{}
+  export default {
+    data(){
+      return {
+        formdata:{
+          username:'',
+          password:'',
+        }
+
+      }
+    }
+  }
 </script>
 
 <style>
